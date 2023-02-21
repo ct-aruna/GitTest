ca2c4e3da4fea7a168b889e702e327a7a77222e8
ct-aruna <aruna.kumari@cartrade.com>
2023-02-21 15:13:24 +0530

    1)merge

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("push to git")
+        print("Test merge")
         // Do any additional setup after loading the view.
     }
 
